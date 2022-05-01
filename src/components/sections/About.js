f713f7fe5a71978d707906ad0dc6354355167919
pulsarforge <omgworkshopfire@gmail.com>
2022-05-01 12:05:29 +0200

    Fix image size and delete arrow

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -25,7 +25,7 @@ function About() {
             <div className="spacer d-md-none d-lg-none" data-height="30"></div>
           </div>
 
-          <div className="col-md-9 triangle-left-md triangle-top-sm">
+          <div className="col-md-9 triangle-top-sm">
             <div className="rounded bg-white shadow-dark padding-30">
               <div className="row">
                 <div className="col-md-10">
