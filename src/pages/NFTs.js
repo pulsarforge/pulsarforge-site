7d6747bd4339baa7b9fe0bb12a841b88f5db1d25
pulsarforge <omgworkshopfire@gmail.com>
2022-05-01 19:36:31 +0200

    Update images and set up gallery

--- src/pages/NFTs.js
+++ src/pages/NFTs.js
@@ -8,7 +8,39 @@ import Footer from "../components/layouts/Footer";
 const filters = [
     {
       id: 1,
-      text: "Voxelverse",
+      text: "Voxelverse All In",
+    },
+    {
+      id: 2,
+      text: "1 to 108",
+    },
+    {
+      id: 3,
+      text: "109 to 217",
+    },
+    {
+      id: 4,
+      text: "218 to 326",
+    },
+    {
+      id: 5,
+      text: "327 to 435",
+    },
+    {
+      id: 6,
+      text: "436 to 544",
+    },
+    {
+      id: 7,
+      text: "545 to 653",
+    },
+    {
+      id: 8,
+      text: "654 to 762",
+    },
+    {
+      id: 9,
+      text: "763 to 871",
     }
   ];
   
@@ -16,121 +48,144 @@ const filters = [
     {
       id: 1,
       title: "The Gathering",
-      category: "Voxelverse",
+      category: "1 to 108",
       image: "images/works/The-Gathering.png",
       popupLink: ["images/works/The-Gathering.png"],
     },
     {
       id: 2,
       title: "Cube Growth Chamber",
-      category: "Voxelverse",
+      category: "1 to 108",
       image: "images/works/Cube-Growth-Chamber.png",
       popupLink: ["images/works/Cube-Growth-Chamber.png"],
     },
     {
       id: 3,
       title: "Spring",
-      category: "Voxelverse",
+      category: "1 to 108",
       image: "images/works/Spring.png",
       popupLink: ["images/works/Spring.png"],
     },
     {
       id: 4,
       title: "Claws",
-      category: "Voxelverse",
+      category: "1 to 108",
       image: "images/works/Bio-Portal.png",
       popupLink: ["images/works/Bio-Portal.png"],
     },
     {
       id: 5,
       title: "Magnetical Landing",
-      category: "Voxelverse",
+      category: "1 to 108",
       image: "images/works/Magnetical-Landing.png",
       popupLink: ["images/works/Magnetical-Landing.png"],
     },
     {
       id: 6,
       title: "Energy Integrator",
-      category: "Voxelverse",
+      category: "1 to 108",
       image: "images/works/Energy-Integrator.png",
       popupLink: ["images/works/Energy-Integrator.png"],
     },
     {
       id: 7,
       title: "Claws",
-      category: "Voxelverse",
+      category: "1 to 108",
       image: "images/works/Claws.png",
       popupLink: ["images/works/Claws.png"],
     },
     {
       id: 8,
       title: "Concentration",
-      category: "Voxelverse",
+      category: "1 to 108",
       image: "images/works/Concentration.png",
       popupLink: ["images/works/Concentration.png"],
     },
     {
       id: 9,
       title: "Risks",
-      category: "Voxelverse",
+      category: "1 to 108",
       image: "images/works/Risks.png",
       popupLink: ["images/works/Risks.png"],
     },
     {
       id: 10,
       title: "Vitamins Receptor",
-      category: "Voxelverse",
+      category: "1 to 108",
       image: "images/works/Vitamins-Receptor.png",
       popupLink: ["images/works/Vitamins-Receptor.png"],
     },
     {
       id: 11,
-      title: "The Ascencion",
-      category: "Voxelverse",
-      image: "images/works/The-Ascencion.png",
-      popupLink: ["images/works/The-Ascencion.png"],
+      title: "The Ascension",
+      category: "1 to 108",
+      image: "images/works/The-Ascension.png",
+      popupLink: ["images/works/The-Ascension.png"],
     },
     {
       id: 12,
       title: "Union Motor",
-      category: "Voxelverse",
+      category: "1 to 108",
       image: "images/works/Union-Motor.png",
       popupLink: ["images/works/Union-Motor.png"],
     },
     {
       id: 13,
       title: "Max Security",
-      category: "Voxelverse",
+      category: "1 to 108",
       image: "images/works/Max-Security.png",
       popupLink: ["images/works/Max-Security.png"],
     },
     {
       id: 14,
       title: "Energy Spreader",
-      category: "Voxelverse",
+      category: "1 to 108",
       image: "images/works/Energy-Spreader.png",
       popupLink: ["images/works/Energy-Spreader.png"],
     },
     {
       id: 15,
       title: "Getting Sun",
-      category: "Voxelverse",
+      category: "1 to 108",
       image: "images/works/Getting-Sun.png",
       popupLink: ["images/works/Getting-Sun.png"],
     },
+    {
+      id: 16,
+      title: "Volcano Phase",
+      category: "1 to 108",
+      image: "images/works/V0016-Volcano-Phase.png",
+      popupLink: ["images/works/V0016-Volcano-Phase.png"],
+    },
+    {
+      id: 17,
+      title: "Growth Diet",
+      category: "1 to 108",
+      image: "images/works/V0017-Growth-Diet.png",
+      popupLink: ["images/works/V0017-Growth-Diet.png"],
+    },
+    {
+      id: 18,
+      title: "Electrical-Way",
+      category: "1 to 108",
+      image: "images/works/V0018-Electrical-Way.png",
+      popupLink: ["images/works/V0018-Electrical-Way.png"],
+    },
   ];
-
+  
+  
+  
 function NFTs() {
     const [getAllItems] = useState(allData);
-    const [dataVisibleCount, setDataVisibleCount] = useState(6);
-    const [dataIncrement] = useState(3);
+    const [dataVisibleCount, setDataVisibleCount] = useState(9);
+    const [dataIncrement] = useState(9);
     const [activeFilter, setActiveFilter] = useState("");
     const [visibleItems, setVisibleItems] = useState([]);
     const [noMorePost, setNoMorePost] = useState(false);
   
     useEffect(() => {
       setActiveFilter(filters[0].text.toLowerCase());
-      setVisibleItems(getAllItems.filter((item) => item.id <= 6));
+      setVisibleItems(getAllItems.filter((item) => item.id <= 9));
     }, [getAllItems]);
   
     const handleChange = (e) => {
