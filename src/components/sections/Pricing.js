790205e85eaf7372be5cd389d0b8de0fbd77c7a2
pulsarforge <omgworkshopfire@gmail.com>
2022-05-16 21:02:27 +0200

    Update text

--- src/components/sections/Pricing.js
+++ src/components/sections/Pricing.js
@@ -11,7 +11,7 @@ const priceboxData = [
     content: [
       {
         id: 0,
-        text: "Reliable and Outstanding Websites",
+        text: "Reliable and Outstanding Websites, with the faster technologies and frameworks",
       },
     ],
     price: "Contact",
@@ -37,7 +37,7 @@ const priceboxData = [
     title: "Smart Contract Development",
     icon: "images/polygon.png",
     content: [
-      { id: 1, text: "Focus on Ethereum and Polygon and We are available for other blockchains." },
+      { id: 1, text: "Focus on Ethereum and Polygon and We are available for other blockchains" },
     ],
     price: "Contact",
     best: false,
