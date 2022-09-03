1fa7dbaeb3b19f1601301e8dd10b903e4006463b
pulsarforge <omgworkshopfire@gmail.com>
2022-09-03 08:59:59 +0200

    Add social to footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -8,6 +8,23 @@ function Footer({ light }) {
           &copy; 2017-2022 PulsarForge all rights reserved.
         </span>
       </div>
+      <ul className="social-icons light list-inline mb-0 mt-4">
+        <li className="list-inline-item">
+          <a href="https://discord.com/invite/frYAsD2DjT" target="_blank" rel="noreferrer">
+            <i className="fab fa-discord adjust-social-icons"></i>
+          </a>
+        </li>
+        <li className="list-inline-item">
+          <a href="https://github.com/pulsarforge" target="_blank" rel="noreferrer">
+            <i className="fab fa-github adjust-social-icons"></i>
+          </a>
+        </li>
+        <li className="list-inline-item">
+          <a href="https://twitter.com/pulsarforge" target="_blank"rel="noreferrer">
+            <i className="fab fa-twitter adjust-social-icons"></i>
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }
