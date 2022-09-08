ae6a75f2a440be213622da48041061e7fddb9954
pulsarforge <omgworkshopfire@gmail.com>
2022-09-08 19:21:22 +0200

    delete elements for better display

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -28,23 +28,6 @@ function Contact() {
                   Send an{" "}
                   <a href="mailto:omgworkshopfire@gmail.com">email</a>. 👋
                 </h2>
-                <ul className="social-icons light list-inline mb-0 mt-4">
-                  <li className="list-inline-item">
-                    <a href="https://discord.com/invite/frYAsD2DjT" target="_blank" rel="noreferrer">
-                      <i className="fab fa-discord adjust-social-icons"></i>
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a href="https://github.com/pulsarforge" target="_blank" rel="noreferrer">
-                      <i className="fab fa-github adjust-social-icons"></i>
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a href="https://twitter.com/pulsarforge" target="_blank"rel="noreferrer">
-                      <i className="fab fa-twitter adjust-social-icons"></i>
-                    </a>
-                  </li>
-                </ul>
               </ScrollAnimation>
             </div>
           </div>
