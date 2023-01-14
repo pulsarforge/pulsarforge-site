65c27443d789fca6b23b52a566aa8d6af1371399
pulsarforge <omgworkshopfire@gmail.com>
2023-01-14 12:56:43 +0100

    Clean, simplify.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import "animate.css/animate.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
