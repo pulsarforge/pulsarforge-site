fc6b5754769c319aa969afc2dd7ac1a71be7935a
pulsarforge <omgworkshopfire@gmail.com>
2023-01-19 19:22:09 +0100

    Update hero section background and words contrast

--- src/components/sections/Herosection.js
+++ src/components/sections/Herosection.js
@@ -13,24 +13,19 @@ function Herosection(props) {
     >
       <div className="container">
         <div className="intro">
-          <img src="images/logo.png" alt="Pulsarforge" className="mb-4" style={{width: "70%"}}/>
-          <h1 className="mb-2 mt-0" style={{fontSize: 70}}>Web3 Labs</h1>
-          <div className="text-center mt-4">
-          <a href="https://discord.com/invite/4mXg6QSyY8" target="_blank" rel="noreferrer"> 
-            <button
-              className="btn btn-default" style={{fontSize: 25}}
-            >
-              Join us at discord
-            </button>
-          </a>
-        </div>
-        <div className="mt-4" style={{paddingBottom: "100px"}}>
-              <h2 style={{fontSize: 35}}>Playground by Pulsarforge</h2>
-              <h3 style={{fontSize: 20}}>Five years at the craft and adding skills along the path <br /> <br /> Exploring the world and scaling projects</h3>
-          </div>
-        </div>
+          <img src="images/logo.png" alt="Pulsarforge" className="mb-4" style={{width: "90%"}}/>
+          <h1 className="mb-2 mt-0" style={{fontSize: 65, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Web3 Labs</h1>
 
-        
+          <div className="mt-4">
+              <h2 style={{fontSize: 40, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Playground by Pulsarforge</h2>
+              <h3 style={{fontSize: 20, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Five years at the craft and adding skills along the path <br /> <br /> Exploring the world and scaling projects</h3>
+          </div> 
+          <div className="text-center mt-4" style={{marginBottom: 300, paddingTop: 10}}>
+            <a href="https://discord.com/invite/4mXg6QSyY8" target="_blank" rel="noreferrer"> 
+              <button className="btn btn-default" style={{fontSize: 25, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Join us at discord</button>
+            </a>
+          </div>
+        </div> 
       </div>
     </section>
   );
