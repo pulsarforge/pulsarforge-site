6bd9c7091142d76c9e59c892f3a222199656dbe9
pulsarforge <omgworkshopfire@gmail.com>
2023-01-21 23:16:19 +0100

    Update hero

--- src/components/sections/Herosection.js
+++ src/components/sections/Herosection.js
@@ -17,15 +17,16 @@ function Herosection(props) {
             { isOpened ? <img src="/images/energy-loop-v2.gif" alt="Energy" className="logo-back-energy"/> : <></>}
         </div>
         <div className="intro">
-          <h1 style={{fontSize: 65, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Web3 Labs</h1>
+          <h1 style={{fontSize: 70, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Web3 Labs</h1>
           <div>
-              <h2 style={{fontSize: 40, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Playground by Pulsarforge</h2>
-              <h3 style={{fontSize: 20, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Five years at the code craft & adding skills along the path <br /> <br /> Exploring the world and scaling projects</h3>
+              <h2 style={{fontSize: 45, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Playground by Pulsarforge O. M. G.</h2>
+              <h3 style={{fontSize: 25, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Five years at the code craft & adding skills along the path<br /><br />Exploring the world and shipping projects</h3>
           </div> 
           <div className="text-center mt-4" style={{marginBottom: 170, paddingTop: 10}}>
             <a href="https://discord.com/invite/4mXg6QSyY8" target="_blank" rel="noreferrer"> 
-              <button className="btn btn-default" style={{fontSize: 25, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Join us at discord</button>
+              <button className="btn btn-default" style={{fontSize: 35, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Join us at discord</button>
             </a>
+            <h3 style={{fontSize: 25, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Actions write the history</h3>
           </div>
         </div> 
       </div>
