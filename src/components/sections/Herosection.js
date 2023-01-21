b4fe068c60cb3262864fa8e60330daab497ec423
pulsarforge <omgworkshopfire@gmail.com>
2023-01-21 02:00:15 +0100

    Add energy to logo

--- src/components/sections/Herosection.js
+++ src/components/sections/Herosection.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Herosection(props) {
+  const [isOpened, setIsOpened] = useState(false);
+
+  function toggle() {
+    setIsOpened(wasOpened => !wasOpened);
+  }
 
   return (
     <section id="home" className={   props.light     ? "home d-flex align-items-center light"     : "home d-flex align-items-center" }>
       <div style={{ background: "url('/images/hero-background-v3.png')", backgroundRepeat: "no-repeat", height: "100%", width: "100%", minHeight: "100%", backgroundSize: "100%"}} >
+        <div className="intro-logo">
+            <button onClick={toggle} style={{ background: "transparent", border: "none", fontSize: 0}} >
+              <img src="images/logo.png" alt="Pulsarforge" style={{width: "40%"}}/>
+            </button>
+            { isOpened ? <img src="/images/energy-loop-v1.gif" alt="Energy" className="logo-back-energy"/> : <></>}
+        </div>
         <div className="intro">
-          <img src="images/logo.png" alt="Pulsarforge" className="mb-4" style={{width: "75%"}}/>
-          <h1 style={{fontSize: 65, paddingTop: 80, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Web3 Labs</h1>
-
-          <div className="mt-4">
+          <h1 style={{fontSize: 65, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Web3 Labs</h1>
+          <div>
               <h2 style={{fontSize: 40, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Playground by Pulsarforge</h2>
               <h3 style={{fontSize: 20, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Five years at the code craft & adding skills along the path <br /> <br /> Exploring the world and scaling projects</h3>
           </div> 
