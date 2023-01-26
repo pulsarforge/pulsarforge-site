766e0e7290a1a4ed21f4d0fae92e774e093a6132
pulsarforge <omgworkshopfire@gmail.com>
2023-01-26 22:19:55 +0100

    Routes consistency and added 404 route

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import Homepage3Dark from "./pages/Homepage3Dark";
 import CivilizationsVoxelverse from "./pages/CivilizationsVoxelverse";
 import Bloglist from "./pages/Bloglist";
 import Articles from "./pages/Articles";
+import NotFoundPage from "./pages/NotFoundPage";
 import "./App.scss";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact>
-          <Homepage3Dark />
-        </Route>
-        <Route path="/civilizations-voxelverse-nfts" exact>
-          <CivilizationsVoxelverse />
-        </Route>
+        <Route path="/" component={Homepage3Dark} exact />
+        <Route path="/civilizations-voxelverse-nfts" component={CivilizationsVoxelverse} exact />
         <Route path="/blogs" component={Bloglist} exact />
         <Route path="/blogs/:id/:title" component={Articles} />
+        <Route component={NotFoundPage} />
       </Switch>
     </BrowserRouter>
   );
