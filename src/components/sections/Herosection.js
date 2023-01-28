be3b983f9b5547261b9d9f9425ff481cc7eb670c
pulsarforge <omgworkshopfire@gmail.com>
2023-01-28 14:22:51 +0100

    delete, rename, move images

--- src/components/sections/Herosection.js
+++ src/components/sections/Herosection.js
@@ -9,12 +9,12 @@ function Herosection(props) {
 
   return (
     <section id="home" className={   props.light     ? "home d-flex align-items-center light"     : "home d-flex align-items-center" }>
-      <div style={{ background: "url('/images/hero-background-v3.png')", backgroundRepeat: "no-repeat", height: "100%", width: "100%", minHeight: "100%", backgroundSize: "100%"}} >
+      <div style={{ background: "url('/images/hero-background.png')", backgroundRepeat: "no-repeat", height: "100%", width: "100%", minHeight: "100%", backgroundSize: "100%"}} >
         <div className="intro-logo">
             <button onClick={toggle} style={{ background: "transparent", border: "none", fontSize: 0}} >
               <img src="images/logo.png" alt="Pulsarforge" style={{width: "40%"}}/>
             </button>
-            { isOpened ? <img src="/images/energy-loop-v2.gif" alt="Energy" className="logo-back-energy"/> : <></>}
+            { isOpened ? <img src="/images/energy-loop.gif" alt="Energy" className="logo-back-energy"/> : <></>}
         </div>
         <div className="intro">
           <h1 style={{fontSize: 70, textShadow: "3px 3px 2px rgb(37, 37, 37)"}}>Web3 Labs</h1>
--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -9,7 +9,7 @@ function NotFoundPage(props) {
     <>
       <Header logoSource="/images/PulsarForge-1.png" />
       <section className={   props.light     ? "home d-flex align-items-center light"     : "home d-flex align-items-center" }>
-        <div style={{ background: "url('/images/hero-background-v3.png')", backgroundRepeat: "no-repeat", height: "100%", width: "100%", minHeight: "100%", backgroundSize: "100%"}} >
+        <div style={{ background: "url('/images/hero-background.png')", backgroundRepeat: "no-repeat", height: "100%", width: "100%", minHeight: "100%", backgroundSize: "100%"}} >
           <div className="intro">
             <div>
                 <h2 style={{fontSize: 32, color: "#fff", textShadow: "3px 3px 2px rgb(37, 37, 37)", paddingTop: 240, paddingBottom: 240}}>Hey It seems you landed in the unknown 404<br /><br />Back to the check the map and get back to the road</h2>
