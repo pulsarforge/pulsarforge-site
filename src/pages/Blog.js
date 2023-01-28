3f218dfd3ecb79bf7269ae5120140fa86a7be94d
pulsarforge <omgworkshopfire@gmail.com>
2023-01-28 09:06:40 +0100

    delete inline styles and page title

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -4,7 +4,6 @@ import BlogElement from "../components/elements/Blog";
 import Pagination from "../components/elements/Pagination";
 import { Helmet } from "react-helmet";
 import Footer from "../components/layouts/Footer";
-import Pagetitle from "../components/elements/Pagetitle";
 import BlogJson from "../data/blog.json";
 
 function Blog() {
@@ -50,13 +49,18 @@ function Blog() {
         toggleMenu={toggleMenu}
         headerToggler={headerToggler}
       />
-      <main style={{backgroundColor: "rgb(24, 24, 24)"}}>
+      <main>
         <div className="spacer" data-height="96"></div>
-        <div className="blog-page-section" style={{backgroundColor: "rgb(24, 24, 24)"}}>
+        <div className="blog-page-section">
           <div className="container">
-            <div className="row blog-wrapper fix-spacing" style={{paddingTop: 35}}>
-              <div style={{marginTop: 40, textAlign: "center"}}>
-                <Pagetitle title="Free Style" />
+            <div className="row blog-wrapper fix-spacing">
+              <div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
               </div>
               {currentPosts.map((blogItem) => (
                 <div className="col-md-3" key={blogItem.id}>
