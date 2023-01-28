2cb32bad8c64f4fb98c41a9c0f399a96ffe6c2f0
pulsarforge <omgworkshopfire@gmail.com>
2023-01-28 11:36:01 +0100

    Upgrade to react 18

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "animate.css/animate.min.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+reportWebVitals();
