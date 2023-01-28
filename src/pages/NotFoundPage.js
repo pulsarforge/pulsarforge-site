f1ec5527ed42aa4daba32fc72527a7101917bd81
pulsarforge <omgworkshopfire@gmail.com>
2023-01-28 09:28:42 +0100

    update style consistency

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -12,7 +12,7 @@ function NotFoundPage(props) {
         <div style={{ background: "url('/images/hero-background-v3.png')", backgroundRepeat: "no-repeat", height: "100%", width: "100%", minHeight: "100%", backgroundSize: "100%"}} >
           <div className="intro">
             <div>
-                <h2 style={{fontSize: 32, color: "white", textShadow: "3px 3px 2px rgb(37, 37, 37)", paddingTop: 240, paddingBottom: 240}}>Hey It seems you landed in the unknown 404<br /><br />Back to the check the map and get back to the road</h2>
+                <h2 style={{fontSize: 32, color: "#fff", textShadow: "3px 3px 2px rgb(37, 37, 37)", paddingTop: 240, paddingBottom: 240}}>Hey It seems you landed in the unknown 404<br /><br />Back to the check the map and get back to the road</h2>
             </div> 
           </div> 
         </div>
