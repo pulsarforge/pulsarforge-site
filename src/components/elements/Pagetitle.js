84962b9758d058a81758f113cae0d3f1066cd656
pulsarforge <omgworkshopfire@gmail.com>
2023-02-03 23:33:29 +0100

    Text consistency

--- src/components/elements/Pagetitle.js
+++ src/components/elements/Pagetitle.js
@@ -3,7 +3,7 @@ import React from "react";
 function Pagetitle({ title }) {
   return (
     <>
-      <h5 className="section-title" style={{color: "#fff"}}>{title}</h5>
+      <h2 className="section-title" style={{fontSize: 44, color: "#fff", textShadow: "3px 3px 2px rgb(37, 37, 37)", fontFamily: "DogBrain"}}>{title}</h2>
       <div className="spacer" data-height="60"></div>
     </>
   );
