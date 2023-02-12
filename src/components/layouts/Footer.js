178456cb24524f282fd6d76b661cf4410029fba4
pulsarforge <omgworkshopfire@gmail.com>
2023-02-12 19:45:31 +0100

    Simplify

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -5,26 +5,9 @@ function Footer({ light }) {
     <footer className={light ? "footer light" : "footer"}>
       <div className="container">
         <h5 className="copyright">
-          &copy; {new Date().getFullYear()} Pulsarforge all rights reserved
+          &copy; {new Date().getFullYear()} Pulsarforge all rights reserved           
         </h5>
       </div>
-      <ul className="social-icons light list-inline mb-0 mt-4 icons-social-footer" >
-        <li className="list-inline-item">
-          <a href="https://discord.com/invite/4mXg6QSyY8" target="_blank" rel="noreferrer">
-            <i className="fab fa-discord adjust-social-icons"></i>
-          </a>
-        </li>
-        <li className="list-inline-item">
-          <a href="https://github.com/pulsarforge" target="_blank" rel="noreferrer">
-            <i className="fab fa-github adjust-social-icons"></i>
-          </a>
-        </li>
-        <li className="list-inline-item">
-          <a href="https://twitter.com/pulsarforge" target="_blank"rel="noreferrer">
-            <i className="fab fa-twitter adjust-social-icons"></i>
-          </a>
-        </li>
-      </ul>
     </footer>
   );
 }
