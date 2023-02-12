c5464c588a5cbcc87559735149556248ac34fecc
pulsarforge <omgworkshopfire@gmail.com>
2023-02-12 19:46:30 +0100

    Simplify

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Pagetitle from "../elements/Pagetitle";
-import ScrollAnimation from "react-animate-on-scroll";
 
 function Contact() {
 
@@ -8,26 +7,12 @@ function Contact() {
     <section id="contact" >
       <div className="container">
         <Pagetitle title="Get in touch" />
-
         <div className="row">
           <div className="col-md-10">
             <div className="contact-info">
-              <ScrollAnimation
-                animateIn="fadeInUp"
-                animateOut="fadeInOut"
-                animateOnce={true}
-              >
-                <h2>For collabs, business, products, and services related talk</h2>
-              </ScrollAnimation>
-              <ScrollAnimation
-                animateIn="fadeInUp"
-                animateOut="fadeInOut"
-                animateOnce={true}
-              >
-                <h2>
-                  <a href="mailto:pulsarforgelabs@gmail.com">Let your message travel 📬</a>
-                </h2>
-              </ScrollAnimation>
+              <h2>
+                <a href="mailto:pulsarforgelabs@gmail.com">Let your message travel 📬</a>
+              </h2>
             </div>
           </div>
         </div>
