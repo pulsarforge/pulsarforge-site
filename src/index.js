56f7c0588df5ceae784ca608c0ca14e6aa2d33e3
pulsarforge <omgworkshopfire@gmail.com>
2023-03-23 00:45:59 +0100

    format

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "animate.css/animate.min.css";
 import App from "./App";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
