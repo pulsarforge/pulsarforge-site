c90f62233d2a74cab23c82a0a066870fc84abfbe
pulsarforge <omgworkshopfire@gmail.com>
2023-03-27 09:33:38 +0200

    Update footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -4,7 +4,7 @@ function Footer({ light }) {
   return (
     <footer id="footer" className={light ? "footer light" : "footer"}>
       <div className="container">
-        <a className="navigate-link" href="#home">
+        <a className="navigate-link" href="#vibes">
           <h5 className="copyright">
             &copy; {new Date().getFullYear()} Pulsarforge all rights reserved
           </h5>
