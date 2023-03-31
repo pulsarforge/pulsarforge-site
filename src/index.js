1abdb29ef521ec459248cc0f480534de7f5db4ba
pulsarforge <omgworkshopfire@gmail.com>
2023-03-31 18:13:24 +0200

    Update bootstrap implementation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 
 const container = document.getElementById("root");
