2f2c2f187fbfbffa9e7e5cab6bff9066564547b4
pulsarforge <omgworkshopfire@gmail.com>
2023-03-31 10:53:04 +0200

    Update url, name consistency

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CivilizationsVoxelverse from "./pages/CivilizationsVoxelverse";
-import DogBlog from "./pages/DogBlog";
+import DogDays from "./pages/DogDays";
 import Articles from "./pages/Articles";
 import Tv from "./pages/Tv";
 import NotFoundPage from "./pages/NotFoundPage";
@@ -16,8 +16,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="civilizations-voxelverse-nfts" element={<CivilizationsVoxelverse />} />
         <Route path="tv" element={<Tv />}  />
-        <Route path="blog" element={<DogBlog />} />
-        <Route path="blog/:id/:title" element={<Articles />} />
+        <Route path="dog-days" element={<DogDays />} />
+        <Route path="dog-days/:id/:title" element={<Articles />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
