5efe5dc05c580e2e00923d72198106fe12738fd5
pulsarforge <omgworkshopfire@gmail.com>
2023-03-31 17:10:17 +0200

    Delete import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "animate.css/animate.min.css";
 import App from "./App";
 
 const container = document.getElementById("root");
