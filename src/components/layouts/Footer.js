1e0c8e228c614bf650105abc9bcbc041cd564ab3
pulsarforge <omgworkshopfire@gmail.com>
2023-03-31 10:51:55 +0200

    Update order

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -4,12 +4,12 @@ function Footer({ light }) {
   return (
     <footer id="footer" className={light ? "footer light" : "footer"}>
       <div className="container">
+        <a className="copyright" href="/dog-days"> </a>
         <a className="navigate-link" href="#vibes">
           <h5 className="copyright">
             &copy; {new Date().getFullYear()} Pulsarforge all rights reserved
           </h5>
         </a>
-        <a className="copyright" href="/blog"> </a>
       </div>
     </footer>
   );
