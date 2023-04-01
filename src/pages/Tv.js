bab71e72b3b6a59b8c585f00f97bb595d5106d76
pulsarforge <omgworkshopfire@gmail.com>
2023-04-01 09:11:37 +0200

    Mobile version

--- src/pages/Tv.js
+++ src/pages/Tv.js
@@ -39,7 +39,7 @@ function Tv() {
         <div className="container">
           <div className="row blog-wrapper fix-spacing">
             <div className="page-title-center">
-              <img  src="/images/dog-streaming.jpg" style={{ height: "500px", width: "500px", borderRadius: "80px", marginBottom: "30px"}} alt="dog-streaming"/>
+              <img  src="/images/dog-streaming.jpg" style={{ height: "auto", width: "auto", borderRadius: "80px", marginBottom: "30px"}} alt="dog-streaming"/>
               <Pagetitle title="Live Streaming" />
               <h4> Wen? </h4>
               <h6> Soon </h6>
