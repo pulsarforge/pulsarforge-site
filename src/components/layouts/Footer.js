f3170d92ee2494dffc211f34ce6b1d58343b106c
pulsarforge <omgworkshopfire@gmail.com>
2023-05-31 17:02:03 +0200

    Simplify footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -4,10 +4,9 @@ function Footer({ light }) {
   return (
     <footer id="footer" className={light ? "footer light" : "footer"}>
       <div className="container">
-        <a className="copyright" href="/dog-days"> </a>
         <a className="navigate-link" href="#vibes">
           <h5 className="copyright">
-          💪🏿👴🏿 &copy; {new Date().getFullYear()} Pulsarforge all rights reserved
+          &copy; {new Date().getFullYear()} Pulsarforge all rights reserved
           </h5>
         </a>
       </div>
